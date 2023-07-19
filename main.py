de5f6e4d00b2cb344ed5ae5295edd6679cd44b92
“ifredom” <ifredomvip@gmail.com>
2023-07-19 15:12:37 +0800

    数据预处理

--- main.py
+++ main.py
@@ -2,15 +2,17 @@
 Author: “ifredom” ifredomvip@gmail.com
 Date: 2023-07-17 18:58:02
 LastEditors: “ifredom” ifredomvip@gmail.com
-LastEditTime: 2023-07-17 22:19:54
+LastEditTime: 2023-07-19 13:34:30
 FilePath: \createVideo\main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
-from src.ge import generate_vlog
+from generate_vlog import generate_vlog
 
 # 设置输入文件路径和输出文件路径
 filename = "assets/input.txt"
 output_path = "dist/output.mp4"
 
-# 调用generate_vlog()函数
-generate_vlog(filename, output_path)
+
+
+if __name__ == '__main__':
+  generate_vlog(filename, output_path)
